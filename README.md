d74fd44b29a8e5f435e81a1f705294fc7c8bbee6
boychapz <germainchi43@gmail.com>
2023-01-02 01:13:43 -0600

    complete js section

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Pixels-app
+
+# URL
+
+# Story
+
+used the pexels api to request JSON with Ajax using the search string as parameter and build a Dynamic Photo Search App with the Pexels API
+
+# screenshot
--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+let API_KEY = "563492ad6f91700001000001d587c600c50145c3b29a7df08505b2b3";
+let SearchForm = document.querySelector('#search-form');
 
-let API_KEY = '563492ad6f91700001000001d587c600c50145c3b29a7df08505b2b3';
+SearchForm.addEventListener('submit', function(e){
+  e.preventDefault();
 
+  var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          //document.getElementById("container").innerHTML = xhttp.responseText;
+          let res = JSON.parse(xhttp.responseText);
+          console.log(res);
+          console.log(res.photos);
+          // let picData= res.photos.map(function(item){
+          //   console.log(item.src.medium)
+          //   return item.src;
+            
+          // });
+          
+          let imageSection = document.getElementById('img-section');
+          res.photos.forEach(function (picture) {
+            let imgDiv = document.createElement('div');
+            imgDiv.classList.add('pic-divs')
+            imgDiv.innerHTML = ` 
+            <img src="${picture.src.medium}">
+            <p> ${picture.photographer}</p>
+            <p>${picture.photographer_url}</p>
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("cover-img").innerHTML = xhttp.responseText;
-      console.log(xhttp.responseText);
-    }
-  };
-  xhttp.open("GET", "https://api.pexels.com/v1/search?query=people", true);
-  xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001d587c600c50145c3b29a7df08505b2b3');
-  xhttp.send();
+            `
+
+            imageSection.appendChild(imgDiv)
+            
+          });
+           
+        }
+      };
+      let textValue = document.getElementById('search-bar').value;
+      
+
+      xhttp.open("GET", `https://api.pexels.com/v1/search?query=${textValue}`, true);
+      xhttp.setRequestHeader("Authorization", API_KEY);
+      xhttp.send();
+
+ 
+})
+      
